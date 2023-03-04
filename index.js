de0fb60c4d6571eae2ae61fd2069e297e17566fc
mario <mario.mazzarelli@asfweb.net>
2023-03-04 17:28:31 +0100

    .

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log('');
+require("dotenv").config();
+const { Client, GatewayIntentBits, Partials } = require("discord.js");
+
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.DirectMessages,
+		GatewayIntentBits.MessageContent,
+	],
+	partials: [Partials.Channel],
+});
+
+client.once("ready", () => {
+	console.log("Ready!");
+});
+
+client.on("interactionCreate", (interaction) => {});
+
+client.on("messageCreate", (message) => {
+	if (isMyBot(message.author.id) == false) {
+		message.react("❤️");
+	}
+	if (message.content === "deploy.now") {
+		message.channel.send("deploy start ....");
+	}
+});
+
+client.login(process.env.BOT_TOKEN).then(() => {});
+
+function isMyBot(id) {
+	return id === process.env.OAUTH_CLIENT_ID;
+}
